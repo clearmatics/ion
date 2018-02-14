345b35d5d9ab7c2390007a012241decfbc22d315
Harry R <HarryR@users.noreply.github.com>
2018-02-14 14:52:07 +0000

    Added 'onchain' ABI command auto-generator

--- ion/args.py
+++ ion/args.py
@@ -3,29 +3,38 @@ from .ethrpc import EthJsonRpc
 from .utils import require, scan_bin
 
 
-def arg_bytes20(ctx, param, value):
+def arg_bytes(ctx, param, value):
     if value is None:
         return None
     value = scan_bin(value)
-    require( len(value) == 20, "20 bytes required" )
     return value
 
+def make_bytes_n(n):
+    def arg_bytes_n(ctx, param, value):
+        value = arg_bytes(ctx, param, value)
+        if value is None:
+            return None
+        require( len(value) == n, str(n) + " bytes required" )
+        return value
+    return arg_bytes_n
 
-def arg_bytes32(ctx, param, value):
-    if value is None:
-        return None
-    value = scan_bin(value)
-    require( len(value) == 32, "32 bytes required" )
-    return value
+
+arg_bytes20 = make_bytes_n(20)
+arg_bytes32 = make_bytes_n(32)
 
 
-def arg_uint256(ctx, param, value):
-    if value is None:
-        return None
-    value = int(value)
-    require( value >= 0 )
-    require( value <= (1<<255) )
-    return value
+def make_uint_n(n):
+    def arg_uint_n(ctx, param, value):
+        if value is None:
+            return None
+        value = int(value)
+        require( value >= 0 )
+        require( value <= (1<<(n-1)) )
+        return value
+    return arg_uint_n
+
+arg_uint256 = make_uint_n(256)
+
 
 def arg_ethrpc(ctx, param, value):
     if value is None:
@@ -35,6 +44,7 @@ def arg_ethrpc(ctx, param, value):
     require( port > 0 )
     require( port < 0xFFFF )
     return EthJsonRpc(ip, port)
+
 
 """
 class PaymentDependency(argparse.Action):
--- ion/onchain/__main__.py
+++ ion/onchain/__main__.py
@@ -1,15 +1,17 @@
+import os
+import glob
 import click
 
-from ..args import arg_ethrpc, arg_bytes20
+from ..args import arg_ethrpc, arg_bytes20, arg_bytes32, arg_uint256, arg_bytes, make_uint_n, make_bytes_n
+from ..crypto import keccak_256
 
-from .erc223 import main as erc223_main
 
 @click.group("onchain", help="On-chain interfaces")
-@click.option('--rpc', '-r', metavar="ip:port", callback=arg_ethrpc, help="Etherum JSON-RPC server", default='127.0.0.1:8545')
-@click.option('--contract', '-c', metavar="0x...20", callback=arg_bytes20, help="Contract address", required=True)
-@click.option('--account', '-a', metavar="0x...20", callback=arg_bytes20, help="Account to use for transactions", required=True)
+@click.option('--rpc', metavar="ip:port", callback=arg_ethrpc, help="Etherum JSON-RPC server", default='127.0.0.1:8545')
+@click.option('--contract', metavar="0x...20", callback=arg_bytes20, help="Contract address")
+@click.option('--account', metavar="0x...20", callback=arg_bytes20, help="Account to use for transactions")
 @click.pass_context
-def commands(ctx, rpc, contract, account):
+def commands(ctx, rpc, contract, account=None):
     """
     :type ctx: click.Context
     """
@@ -18,8 +20,64 @@ def commands(ctx, rpc, contract, account):
     ctx.meta['account'] = account
 
 
-commands.add_command(erc223_main, "erc223")
+def subcmd(abi_file):
+    import json
+    with open(abi_file, 'rb') as handle:
+        data = json.load(handle)
 
+        grou_name = os.path.basename(abi_file).split('.')[0]
+        group = click.Group(grou_name )
+
+        for method in data:
+            if method['type'] != 'function':
+                continue
+            if method['name'][0] == '_':
+                continue
+
+            argsig = "%s" % (','.join([i['type'] for i in method['inputs']]))
+            sig = "%s(%s)" % (method['name'], argsig)
+            sig_hash = keccak_256(bytes(sig)).hexdigest()[:8]
+
+            @click.command(method['name'], help=sig, short_help=argsig)
+            def cmd():
+                pass
+
+            argtypes = {
+                'bytes': arg_bytes,
+                'address': arg_bytes20,
+                'bytes32': arg_bytes32,
+                'uint256': arg_uint256,
+            }
+            makers = {
+                'uint': make_uint_n,
+                'bytes': make_bytes_n,
+            }
+            for arg in method['inputs']:
+                kwa = dict(
+                    required=True,
+                    metavar=arg['type'],
+                )
+                arg_type = arg['type']
+                if arg_type.endswith("[]"):
+                    arg_type = arg_type[:-2]
+                    kwa['multiple'] = True
+
+                without_ints = arg_type.rstrip('0123456789')
+                if without_ints != arg_type:
+                    n = int(arg_type[len(without_ints):])
+                    kwa['type'] = makers[without_ints](n)
+                else:
+                    kwa['type'] = argtypes[arg_type]
+
+                name = arg['name'].strip('_')
+                cmd = click.option('--' + name, **kwa)(cmd)
+
+            group.add_command(cmd)
+        commands.add_command(group)
+
+
+for filename in glob.glob('abi/*.abi'):
+    subcmd(filename)
 
 if __name__ == "__main__":
     commands()
