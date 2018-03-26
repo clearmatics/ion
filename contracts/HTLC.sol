3076618b4398e64c3a072988bc420e518b21a25f
fm.grabner <mgb@clearmatics.com>
2018-03-26 11:01:59 +0100

    Added some HTLC tests, it works given previous implementation but I am not sure about the sanity of the refund procedure

--- contracts/HTLC.sol
+++ contracts/HTLC.sol
@@ -5,38 +5,54 @@ pragma solidity ^0.4.18;
 */
 contract HTLC
 {
+
+	event OnDeposit( uint256 lock_id, address receiver );
+	event OnClaim( uint256 lock_id, bytes32 preimage, uint256 timeout );
+	event OnRecover( address receiver, address lockreceiver, address msgsender );
+	event OnVerification( address verified );
+	event Test( uint256 lock_id, uint256 timeout );
+
 	uint256 constant MAX_TIMEOUT = 60 * 60 * 24;
 
 	struct LockState {
-		uint timeout;
+		uint256 timeout;
 		bytes32 hash;
-		address recipient;
+		address receiver;
 		address owner;
 		uint256 value;
 	}
-
 	mapping(uint256 => LockState) internal m_locks;
 
 	uint256 internal m_ctr;
 
-
-	event OnDeposit( uint256 lock_id, address recipient );
-
-	event OnClaim( uint256 lock_id, bytes32 preimage );
-
+  modifier fundsSent() {
+      require(msg.value > 0);
+      _;
+  }
+  modifier futureTimelock(uint _time) {
+      // only requirement is the timelock time is after the last blocktime (now).
+      // probably want something a bit further in the future then this.
+      // but this is still a useful sanity check:
+      require(_time > now);
+      _;
+  }
 
 	/**
 	* Alice deposits her funds (the Input, or side A)
 	*
-	* These can only be withdrawn by the intended `recipient` if they provide
+	* These can only be withdrawn by the intended `receiver` if they provide
 	* the `preimage` which results in `hash`.
 	*
 	* On side `B` alice reveals her secret `preimage`
 	*
 	* If the timeout has been reached Alice (the `owner`) can request a Refund.
 	*/
-	function Deposit (uint256 timeout, bytes32 hash, address recipient)
-		public payable returns (uint256)
+	function Deposit (uint256 timeout, bytes32 hash, address receiver)
+		external
+		payable
+		fundsSent
+		futureTimelock(timeout)
+		returns (uint256)
 	{
 		require( timeout >= now );
 		require( timeout < (now + MAX_TIMEOUT) );
@@ -44,9 +60,9 @@ contract HTLC
 
 		var lock_id = m_ctr;
 		m_ctr += 1;
-		m_locks[lock_id] = LockState(timeout, hash, recipient, msg.sender, msg.value);
+		m_locks[lock_id] = LockState(timeout, hash, receiver, msg.sender, msg.value);
 
-		OnDeposit(lock_id, recipient);
+		OnDeposit(lock_id, receiver);
 
 		return lock_id;
 	}
@@ -55,34 +71,39 @@ contract HTLC
 	function Claim (uint256 lock_id, bytes32 preimage, uint8 v, bytes32 r, bytes32 s)
 		public
 	{
-		bytes32 result = keccak256("randomhash");
+		/* bytes32 result = sha256("randomhash"); */
 		LockState storage lock = m_locks[lock_id];
-
 		require( lock.timeout >= now );
-		require( keccak256(preimage) == lock.hash );
+		require( sha256(preimage) == lock.hash );
 
-        // Only recipient can provide the preimage to withdraw
-		address recipient = ecrecover(keccak256(lock_id, msg.sender), v, r, s);
-		require( recipient == lock.recipient );
+    // Only receiver can provide the preimage to withdraw
+		bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+		bytes32 prefixedHash = keccak256(prefix, preimage);
+		address receiver = ecrecover(prefixedHash, v, r, s);
+		OnVerification(receiver);
+		require( receiver == lock.receiver );
 
 		var value = lock.value;
 		delete m_locks[lock_id];
 
 		msg.sender.transfer(value);
 
-		OnClaim(lock_id, preimage);
+		OnClaim(lock_id, preimage, lock.timeout);
 	}
 
 
-	function Refund (uint256 lock_id, uint8 v, bytes32 r, bytes32 s)
+	function Refund (uint256 lock_id,  bytes32 preimage, uint8 v, bytes32 r, bytes32 s)
 		public
 	{
 		LockState storage lock = m_locks[lock_id];
-
 		require( lock.timeout < now );
+		/* Test(lock.timeout, now); */
 
 		// Lock owner must authorise msg.sender to retrieve refund
-		address owner = ecrecover(keccak256(lock_id, msg.sender), v, r, s);
+		bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+		bytes32 prefixedHash = keccak256(prefix, preimage);
+		address owner = ecrecover(prefixedHash, v, r, s);
+		OnVerification(owner);
 		require( owner == lock.owner );
 
 		var value = lock.value;
@@ -90,4 +111,16 @@ contract HTLC
 
 		msg.sender.transfer(value);
 	}
+
+
+	function Verify(bytes32 hash, uint8 v, bytes32 r, bytes32 s)
+	public
+	{
+		bytes memory prefix = "\x19Ethereum Signed Message:\n32";
+		bytes32 prefixedHash = keccak256(prefix, hash);
+		address receiver = ecrecover(prefixedHash, v, r, s);
+
+		OnVerification(receiver);
+	}
+
 }
---
+++ test/HTLC.js
@@ -0,0 +1,190 @@
+'use strict';
+
+const BigNumber = web3.BigNumber;
+
+const should = require('chai')
+    .use(require('chai-as-promised'))
+    .use(require('chai-bignumber')(BigNumber))
+    .should();
+
+const assert = require('assert');
+
+const HTLC = artifacts.require("HTLC");
+
+const crypto = require('crypto')
+
+// Format required for sending bytes through eth client:
+//  - hex string representation
+//  - prefixed with 0x
+const bufToStr = b => '0x' + b.toString('hex')
+
+const sha256 = x =>
+  crypto
+    .createHash('sha256')
+    .update(x)
+    .digest()
+
+const random32 = () => crypto.randomBytes(32)
+
+const isSha256Hash = hashStr => /^0x[0-9a-f]{64}$/i.test(hashStr)
+
+const newSecretHashPair = () => {
+  const secret = random32()
+  const hash = sha256(secret)
+  return {
+    secret: bufToStr(secret),
+    hash: bufToStr(hash),
+  }
+}
+
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const gasPrice = 100000000000 // truffle fixed gas price
+const txGas = txReceipt => txReceipt.receipt.gasUsed * gasPrice
+const txLoggedArgs = txReceipt => txReceipt.logs[0].args
+const txContractId = txReceipt => txLoggedArgs(txReceipt).contractId
+const oneFinney = web3.toWei(1, 'finney')
+
+
+contract('HTLC', (accounts) => {
+  	let htlc;
+    let htlc_owner;
+
+    beforeEach(async function() {
+		htlc = await HTLC.new();
+    });
+
+    const sender = accounts[1]
+    const receiver = accounts[2]
+    const date = Math.floor(Date.now() / 1000);
+
+    it("Deposit(): Testing the depositing of funds", async function()
+    {
+        const hashPair = newSecretHashPair()
+        const timeout = date + 10;
+        const txReceipt = await htlc.Deposit(
+          timeout,
+          hashPair.hash,
+          receiver,
+          {
+            value: oneFinney,
+            from: sender
+          }
+        )
+        const logArgs = txLoggedArgs(txReceipt)
+
+        assert.equal(logArgs.lock_id.toNumber(), 0);
+        assert.equal(logArgs.receiver, receiver);
+
+    });
+
+    it("Claim(): Should send receiver funds when they give the correct preimage", async function()
+    {
+        const hashPair = newSecretHashPair()
+        const timeout = date + 10;
+        const newContractTx = await htlc.Deposit(
+          timeout,
+          hashPair.hash,
+          receiver,
+          {
+            value: oneFinney,
+            from: sender
+          }
+        )
+
+        const receiverBalBefore = web3.eth.getBalance(receiver)
+        const contractArgs = txLoggedArgs(newContractTx)
+
+        // Posit a claim
+        var sig = web3.eth.sign(receiver, hashPair.secret).slice(2)
+        let r = '0x' + sig.substr(0, 64)
+        let s = '0x' + sig.substr(64, 64)
+        let v = web3.toDecimal(sig.slice(128, 130)) + 27
+
+        const claimTx = await htlc.Claim(
+          contractArgs.lock_id,
+          hashPair.secret,
+          v,
+          r,
+          s,
+          {
+            from: receiver
+          }
+        )
+        const expectedBal = receiverBalBefore
+          .plus(oneFinney)
+          .minus(txGas(claimTx))
+
+        const logArgs = txLoggedArgs(claimTx)
+        const currentBal = web3.eth.getBalance(receiver)
+        assert.equal(logArgs.verified, receiver)
+        assert.equal(currentBal.c[0], expectedBal.c[0]) // check gas cost
+        assert.equal(currentBal.c[1], expectedBal.c[1]) // check balances
+
+    });
+
+    it("Refund(): Should return sender funds when they give the correct preimage", async function()
+    {
+        const hashPair = newSecretHashPair()
+        const timeout = date + 2;
+        const newContractTx = await htlc.Deposit(
+          timeout,
+          hashPair.hash,
+          receiver,
+          {
+            value: oneFinney,
+            from: sender
+          }
+        )
+
+        const senderBalBefore = web3.eth.getBalance(sender)
+        const contractArgs = txLoggedArgs(newContractTx)
+
+        // Wait sometime to ensure timeout is passed
+        await sleep(3000)
+
+        // Posit a claim
+        var sig = web3.eth.sign(sender, hashPair.secret).slice(2)
+        let r = '0x' + sig.substr(0, 64)
+        let s = '0x' + sig.substr(64, 64)
+        let v = web3.toDecimal(sig.slice(128, 130)) + 27
+
+        const refundTx = await htlc.Refund(
+          contractArgs.lock_id,
+          hashPair.secret,
+          v,
+          r,
+          s,
+          {
+            from: sender
+          }
+        )
+        const expectedBal = senderBalBefore
+          .plus(oneFinney)
+          .minus(txGas(refundTx))
+
+        const logArgs = txLoggedArgs(refundTx)
+        const currentBal = web3.eth.getBalance(sender)
+        assert.equal(logArgs.verified, sender)
+        assert.equal(currentBal.c[0], expectedBal.c[0]) // check gas cost
+        assert.equal(currentBal.c[1], expectedBal.c[1]) // check balances
+
+    });
+
+    it('Verify(): ecrecover result matches address', async function() {
+      var msg = 'hello'
+      var h = web3.sha3(msg)
+      var sig = web3.eth.sign(receiver, h).slice(2)
+      var r = '0x' + sig.substr(0, 64)
+      var s = '0x' + sig.substr(64, 64)
+      var v = web3.toDecimal(sig.slice(128, 130)) + 27
+
+      var result = await htlc.Verify(h, v, r, s)
+      const logArgs = txLoggedArgs(result)
+
+      assert.equal(logArgs.verified, receiver)
+    })
+
+});
