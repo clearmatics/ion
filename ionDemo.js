3259162018da5cab8e9780f84779235a03c55122
Duarte Aragao <daragao@gmail.com>
2018-04-17 11:43:07 +0100

    added compile to package, added halt unitl keypress

--- ionDemo.js
+++ ionDemo.js
@@ -1,6 +1,8 @@
+const readline = require('readline');
+
 const Web3 = require('web3')
 
-const deployContract = (contractPath, ownerAcc, args) => {
+const deployContract = (web3, contractPath, ownerAcc, args) => {
   const contractData = require(contractPath)
   const abi = contractData.abi
   const bytecode = contractData.bytecode
@@ -8,23 +10,82 @@ const deployContract = (contractPath, ownerAcc, args) => {
   const txObj = {data: bytecode, from: ownerAcc, gas: '1000000'}
   //const contractInstance = contract.new(...(args || []), txObj)
   //return contractInstance
-  const contractPromise = new Promise((resolve, reject) => contract.new(...(args || []), txObj, (err,contractInstance) => {
-    if(err) reject(err)
-    if(contractInstance.address) resolve(contractInstance)
-  }))
+  const contractPromise =
+    new Promise((resolve, reject) => contract.new(...(args || []), txObj, (err,contractInstance) => {
+      if(err) reject(err)
+      if(contractInstance.address) resolve(contractInstance)
+    }))
   return contractPromise
 }
 
-const loadContract = (address,abiPath) => {
-  const abi = require(abiPath).abi
-  const contract = web3.eth.contract(abi);
-  const instance = contract.at(address)
-  return instance
+const mintToken = async (token, owner, value) => {
+  const mintTxObj = { from: owner, gas: '1000000' }
+  const mintTxHash = await token.mint(value, mintTxObj)
+
+  printBlock([
+    ['Minted tokens to',owner],
+    ['Value:',value],
+    ['Mint TxHash:',mintTxHash],
+  ])
+
+  return mintTxHash
 }
 
-const waitForKeypress = () => {
+const transferToken = async (token, from, to, value, reference) => {
+  const transfer = token.transfer['address,uint256,bytes']
+  const transferTxObj = { from, gas: '1000000' }
+  const transferTxHash = await transfer(to, value, reference, transferTxObj)
+
+  printBlock([
+    ['Transfered tokens from',from,'to',to],
+    ['Value:', value],
+    ['Reference:',reference],
+    ['Transfer TxHash:',transferTxHash]
+  ])
+
+  return transferTxHash
+}
+
+const getIonLockEvent = async (web3, ionLock, reference) => {
+  const refHash = web3.sha3(reference)
+  const lockTransferEvent = ionLock.IonTransfer({ ref: refHash }) // filter by reference
+  const promiseCallback = (resolve,reject) => {
+    lockTransferEvent.get((err,result) => err ? reject(err) : resolve(result))
+  }
+  const lockEvent = await new Promise(promiseCallback)
+
+  const printArr = lockEvent.reduce((prev,le) => prev.concat([
+    ['IonLock event triggered'],
+    ['Name:',le.event],
+    ['TxHash:', le.transactionHash],
+    ['Arguments of event:'],
+    ['\tRecipient:',le.args._recipient],
+    ['\tCurrency hash:',le.args._currency],
+    ['\tValue:',le.args.value.toString()],
+    ['\tReference hash:',le.args.ref],
+    ['\tData (hex of reference):',le.args.data],
+  ]), [])
+  printBlock(printArr)
+
+  return lockEvent
+}
+
+const getWeb3 = (providerURL) => {
+  return new Web3(new Web3.providers.HttpProvider(providerURL))
+}
+
+const waitForKeypress = async () => {
+  readline.emitKeypressEvents(process.stdin)
+  process.stdin.setRawMode(true)
+  const promiseKey = new Promise((resolve, reject) => process.stdin.on('keypress', (str, key) => {
+    if (key.ctrl && key.name === 'c') {
+      process.exit()
+    } else {
+      resolve(key)
+    }
+  }))
   console.log('\n=== Press any key to continue ===\n')
-  // TODO: HALT UNTIL KEYPRESS
+  return promiseKey
 }
 
 const printBlock = (args) => {
@@ -34,78 +95,53 @@ const printBlock = (args) => {
 }
 
 const main = async () => {
-  if (typeof web3 !== 'undefined') {
-    web3 = new Web3(web3.currentProvider);
-  } else {
-    // set the provider you want from Web3.providers
-    const providerURL = 'http://localhost:8545'
-    web3 = new Web3(new Web3.providers.HttpProvider(providerURL));
-  }
+  const web3A = getWeb3('http://localhost:8545')
+  //const web3B = getWeb3('http://localhost:8546')
 
-  const owner = web3.eth.accounts[0]
-  const accountA = web3.eth.accounts[1]
-  const accountB = web3.eth.accounts[2]
+  const owner = web3A.eth.accounts[0]
+  const accountA = web3A.eth.accounts[1]
+  //const accountB = web3A.eth.accounts[2]
 
-  // expects the contracts to have been deployed previously
-  //const token = loadContract('0x9561c133dd8580860b6b7e504bc5aa500f0f06a7','./build/contracts/Token.json')
-  //const ionLink = loadContract('0xc89ce4735882c9f0f0fe26686c53074e09b0d550','./build/contracts/IonLink.json')
-  //const ionLock = loadContract('0xe982e462b094850f12af94d21d470e21be9d0e9c','./build/contracts/IonLock.json')
-  const token = await deployContract('./build/contracts/Token.json', owner)
-  const ionLink = await deployContract('./build/contracts/IonLink.json', owner)
-  const ionLock = await deployContract('./build/contracts/IonLock.json', owner, [token.address, ionLink.address])
+  // deploy contracts
+  const tokenA = await deployContract(web3A, './build/contracts/Token.json', owner)
+  const ionLinkA = await deployContract(web3A, './build/contracts/IonLink.json', owner)
+  const ionLockA = await deployContract(web3A, './build/contracts/IonLock.json', owner, [tokenA.address, ionLinkA.address])
 
   printBlock([
     ['Deployed Contracts'],
-    ['Token:',token.address],
-    ['IonLock:',ionLock.address],
-    ['IonLink:',ionLink.address]])
+    ['Token:',tokenA.address],
+    ['IonLock:',ionLockA.address],
+    ['IonLink:',ionLinkA.address]])
 
-  waitForKeypress()
+  //TODO: PRINT BALANCES WHEN RELEVANT
+
+  await waitForKeypress()
 
   const value = 1000
   const reference = 'Example A'
-  const mintTxHash = await token.mint(value,{ from: owner, gas: '1000000' })
-  const transferTxHash = await token.transfer['address,uint256,bytes'](accountA, value, reference, { from: owner, gas: '1000000' })
 
-  printBlock([
-    ['Minted tokens to Owner and transfered from Owner to A'],
-    ['Owner Account:', owner],
-    ['Account A:', accountA],
-    ['Value:',value],
-    ['Mint TxHash:',mintTxHash],
-    ['Transfer TxHash:',transferTxHash]
-  ])
+  console.log('Mint tokens into the Owner account')
+  const mintTxHash = await mintToken(tokenA, owner, value)
 
-  waitForKeypress()
+  console.log('Transfer tokens from Owner account to Account A')
+  const transferTxHash = await transferToken(tokenA, owner, accountA, value, reference)
+
+  await waitForKeypress()
 
   // setup filter to get ionlock event
-  const lockTxHash = await token.transfer['address,uint256,bytes'](ionLock.address, value, reference, { from: accountA, gas: '1000000' })
+  console.log('Transfer tokens Account A to IonLock')
+  const lockTxHash = await transferToken(tokenA, accountA, ionLockA.address, value, reference)
 
-  printBlock([
-    ['Transfered tokens from A to IonLock'],
-    ['Account A:', accountA],
-    ['IonLock:', ionLock.address],
-    ['Value:',value],
-    ['Reference:',reference],
-    ['Transfer to IonLock TxHash:',lockTxHash],
-  ])
-
-  waitForKeypress()
+  await waitForKeypress()
 
   // Wait for IonLock Event
-  const lockTransferEvent = ionLock.IonTransfer({ ref: web3.sha3(reference) }) // filter by reference
-  const lockEvent = await new Promise((resolve,reject) => lockTransferEvent.get((err,result) => err ? reject(err) : resolve(result)))
+  console.log('IonLock event triggered')
+  const lockEvent = await getIonLockEvent(web3A, ionLockA, reference)
 
-  printBlock([
-    ['IonLock event triggered'],
-    ['TxHash:', lockEvent[0].transactionHash],
-    ['Arguments of event:'],
-    ['\tRecipient:',lockEvent[0].args._recipient],
-    ['\tCurrency hash:',lockEvent[0].args._currency],
-    ['\tValue:',lockEvent[0].args.value.toString()],
-    ['\tReference hash:',lockEvent[0].args.ref],
-    ['\tData (hex of reference):',lockEvent[0].args.data],
-  ])
+
+  // TODO: WAIT FOR UPDATE IN IONLINK
+  // TODO: WITHDRAW
+  process.exit()
 }
 
 try {
