0e0aa263c6e19a0d03f2a3d0b25dc83d28005d11
Chris Chung <cc@clearmatics.com>
2018-04-23 11:42:21 +0100

    Added IonLinkUpdated event when ionlink is updated. Event is used by Frontend for demonstration purposes.

--- test/link.js
+++ test/link.js
@@ -65,6 +65,9 @@ contract('IonLink', (accounts) => {
     const latestBlock = await ionLink.GetLatestBlock()
     const valid = await ionLink.Verify(latestBlock,leafHash,path)
     assert(valid,'IonLink.verify() failed!')
+
+    assert( receiptUpdate.logs.length > 0)
+    assert.equal( receiptUpdate.logs[0].event, 'IonLinkUpdated' )
   })
 
   it('duplicate root', async () => {
