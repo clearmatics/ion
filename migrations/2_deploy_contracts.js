95cd32534c149b4fe44b147deea28fbbc3073c7d
maxrobot <max@grabner.co.uk>
2018-05-03 08:47:35 +0100

    Fixed the deployment issue

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,13 +4,11 @@ const Token = artifacts.require("Token");
 
 module.exports = async (deployer) => {
   try {
-    await deployer.deploy(IonLink, 0);
-
-    await deployer.deploy(Token)
-
-    const ionlink_deployed = await IonLink.deployed();
-    const token_deployed = await Token.deployed();
-    await deployer.deploy(IonLock, token_deployed.address, ionlink_deployed.address);
+    deployer.deploy(IonLink, 0)
+      .then(() => IonLink.deployed)
+      .then(() => deployer.deploy(Token))
+      .then(() => Token.deployed)
+      .then(() => deployer.deploy(IonLock, Token.address, IonLink.address))
 
   } catch(err) {
     console.log('ERROR on deploy:',err);
