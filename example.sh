77fec365ea1479461b4a8dc2bf0cd8520cea9a55
Duarte Aragao <daragao@gmail.com>
2018-05-04 15:42:52 +0100

    easier example.sh to edit

--- example.sh
+++ example.sh
@@ -1,33 +1,44 @@
 #!/bin/bash
+
+ACC_A=0x22d491bde2303f2f43325b2108d26f1eaba1e32b
+ACC_B=0xffcf8fdee72ac11b5c542428b35eef5769c409f0
+TOKEN_ADDR=0x254dffcd3277c0b1660f6d42efbb754edababc2b
+LOCK_ADDR=0xc89ce4735882c9f0f0fe26686c53074e09b0d550
+LINK_ADDR=0xcfeb869f69431e42cdb54a4f4f105c19c080a601
+PORT_A=8545
+PORT_B=8546
+IP_A=127.0.0.1
+IP_B=127.0.0.1
+
 echo "==== Chain A ===="
 echo "Minting"
-python -mion ion mint --rpc 127.0.0.1:8545 --account 0x22d491bde2303f2f43325b2108d26f1eaba1e32b --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000
+python -mion ion mint --rpc $IP_A:$PORT_A --account $ACC_A --tkn $TOKEN_ADDR --value 5000
 read enter
 
 echo "Depositing"
-python -mion ion deposit --rpc 127.0.0.1:8545 --account 0x22d491bde2303f2f43325b2108d26f1eaba1e32b --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion deposit --rpc $IP_A:$PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
 read enter
 
 echo "Fetching proof"
-python -mion ion proof --rpc 127.0.0.1:8545 --account 0x22d491bde2303f2f43325b2108d26f1eaba1e32b --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion proof --rpc $IP_A:$PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
 read enter
 
 echo "==== Chain B ===="
 echo "Minting"
-python -mion ion mint --rpc 127.0.0.1:8546 --account 0xffcf8fdee72ac11b5c542428b35eef5769c409f0 --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000
+python -mion ion mint --rpc $IP_B:$PORT_B --account $ACC_B --tkn $TOKEN_ADDR --value 5000
 read enter
 
 echo "Depositing"
-python -mion ion deposit --rpc 127.0.0.1:8546 --account 0xffcf8fdee72ac11b5c542428b35eef5769c409f0 --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion deposit --rpc $IP_B:$PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
 read enter
 
 echo "Fetching proof"
-python -mion ion proof --rpc 127.0.0.1:8546 --account 0xffcf8fdee72ac11b5c542428b35eef5769c409f0 --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion proof --rpc $IP_B:$PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
 read enter
 
 echo "==== Withdrawing from Chain A ===="
-python -mion ion withdraw --rpc-from 127.0.0.1:8546 --rpc-to 127.0.0.1:8545 --account 0xffcf8fdee72ac11b5c542428b35eef5769c409f0 --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion withdraw --rpc-from $IP_B:$PORT_B --rpc-to $IP_A:$PORT_A --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
 read enter
 
 echo "==== Withdrawing from Chain B ===="
-python -mion ion withdraw --rpc-from 127.0.0.1:8545 --rpc-to 127.0.0.1:8546 --account 0x22d491bde2303f2f43325b2108d26f1eaba1e32b --lock 0xd833215cbcc3f914bd1c9ece3ee7bf8b14f841bb --tkn 0xc89ce4735882c9f0f0fe26686c53074e09b0d550 --value 5000 --ref stuff
+python -mion ion withdraw --rpc-from $IP_A:$PORT_A --rpc-to $IP_B:$PORT_B --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
