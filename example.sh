8c79462aa968eba8e5aa410a01c9168d6b50aafd
Harry <gpg-dev-cm@midnight-labs.org>
2018-06-18 22:13:32 +0100

    example.sh uses a random value, fixed bug with test

--- example.sh
+++ example.sh
@@ -12,54 +12,55 @@ IP_B=127.0.0.1
 API_PORT_A=8555
 API_PORT_B=8556
 REF=`openssl rand 32 | sha256sum | cut -f 1 -d ' '`
+VALUE=$(( ( RANDOM % 10000 ) + 1000 ))
 
-echo python3 -mion lithium --from-account $ACC_A --to-account $ACC_B --rpc-from $IP_A:$PORT_A --rpc-to $IP_B:$PORT_B --lock $LOCK_ADDR --link $LINK_ADDR --api-port $API_PORT_A
-echo python3 -mion lithium --from-account $ACC_A --to-account $ACC_B --rpc-from $IP_B:$PORT_B --rpc-to $IP_A:$PORT_A --lock $LOCK_ADDR --link $LINK_ADDR --api-port $API_PORT_B
+echo $PYTHON -mion lithium --from-account $ACC_A --to-account $ACC_B --rpc-from $IP_A:$PORT_A --rpc-to $IP_B:$PORT_B --lock $LOCK_ADDR --link $LINK_ADDR --api-port $API_PORT_A
+echo $PYTHON -mion lithium --from-account $ACC_A --to-account $ACC_B --rpc-from $IP_B:$PORT_B --rpc-to $IP_A:$PORT_A --lock $LOCK_ADDR --link $LINK_ADDR --api-port $API_PORT_B
 read enter
 
 echo "==== Chain A ===="
 echo "...Minting"
-$PYTHON -mion ion mint --rpc $IP_A:$PORT_A --account $ACC_A --tkn $TOKEN_ADDR --value 5000
+$PYTHON -mion ion mint --rpc $IP_A:$PORT_A --account $ACC_A --tkn $TOKEN_ADDR --value $VALUE
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "...Depositing"
-$PYTHON -mion ion deposit --rpc $IP_A:$PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref $REF
+$PYTHON -mion ion deposit --rpc $IP_A:$PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "...Fetching proof"
-$PYTHON -mion ion proof --lithium-port $API_PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref $REF
+$PYTHON -mion ion proof --lithium-port $API_PORT_A --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "==== Chain B ===="
 echo "...Minting"
-$PYTHON -mion ion mint --rpc $IP_B:$PORT_B --account $ACC_B --tkn $TOKEN_ADDR --value 5000
+$PYTHON -mion ion mint --rpc $IP_B:$PORT_B --account $ACC_B --tkn $TOKEN_ADDR --value $VALUE
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "...Depositing"
-$PYTHON -mion ion deposit --rpc $IP_B:$PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
+$PYTHON -mion ion deposit --rpc $IP_B:$PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "...Fetching proof"
-$PYTHON -mion ion proof --lithium-port $API_PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref stuff
+$PYTHON -mion ion proof --lithium-port $API_PORT_B --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "==== Withdrawing from Chain A ===="
-$PYTHON -mion ion withdraw --lithium-port $API_PORT_B --rpc $IP_A:$PORT_A --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref $REF
+$PYTHON -mion ion withdraw --lithium-port $API_PORT_B --rpc $IP_A:$PORT_A --account $ACC_B --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
 echo ""
 echo "Press any key to proceed"
 read enter
 
 echo "==== Withdrawing from Chain B ===="
-$PYTHON -mion ion withdraw --lithium-port $API_PORT_A --rpc $IP_B:$PORT_B --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value 5000 --ref $REF
+$PYTHON -mion ion withdraw --lithium-port $API_PORT_A --rpc $IP_B:$PORT_B --account $ACC_A --lock $LOCK_ADDR --tkn $TOKEN_ADDR --value $VALUE --ref $REF
