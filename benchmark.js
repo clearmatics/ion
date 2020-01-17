3578b4458a733fb1a995c15dd4c62c5e6d94244c
andrea <dinennoandrea@gmail.com>
2020-01-17 15:48:30 +0000

    Group count opcodes

--- benchmark.js
+++ benchmark.js
@@ -27,6 +27,7 @@ aggregate = (txTrace, name, options) => {
         for (log of txTrace.structLogs) {
             if (log.stack.length > aggregateObj.maxStackDepth)
                 aggregateObj.maxStackDepth = log.stack.length
+
         }
     } else if(!options.disableMemory){
         // memory max depth
@@ -36,23 +37,38 @@ aggregate = (txTrace, name, options) => {
         }
     }
 
-    // write final object containing aggregate
-    data = fs.readJsonSync("./gas-external.json")
-    data[name] = aggregateObj
-    fs.writeJsonSync("./gas-external.json", data)
+    // word count the opcodes
+    for (log of txTrace.structLogs) {
+        aggregateObj[log.op] = aggregateObj[log.op] ? aggregateObj[log.op] += 1 : 1
+    }
+
+    fs.ensureFile(FILEPATH)
+    .then(() => {
+      data = fs.readFileSync(FILEPATH)
+      data = data.length === 0 ? {} : JSON.parse(data)
+      data[name] = aggregateObj
+      
+      fs.writeJsonSync(FILEPATH, data)
+    })
+    .catch(err => {
+      console.error(err)
+    })
 }
 
 // ENTRYPOINT 
 const web3 = new Web3();
 web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545', {timeout:100000}));
 
-options = {disableStorage:true, disableStack:false, disableMemory:true}
+const FILEPATH = "./gas-public.json"
 
-data = fs.readJsonSync("./txMaps.json")
+options = {disableStorage:true, disableStack:true, disableMemory:true}
 
-for (var key of Object.keys(data)) {
-    benchmarkTx(data[key], key, options, web3)
-}
+// data = fs.readJsonSync("./txMaps.json")
 
+// for (var key of Object.keys(data)) {
+//     benchmarkTx(data[key], key, options, web3)
+// }
+
+benchmarkTx("0x22eb8a81dd3d949b2e36e38a9f5221c399c3faf0b4d07e56ca3ee8673a97fa7b", "register", options, web3)
 
 module.exports = benchmarkTx
