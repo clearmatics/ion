c82b06626cdd0477927d3cd867cd43ba1df25f8c
andrea <dinennoandrea@gmail.com>
2020-01-27 14:01:54 +0000

    Integrate the benchmarking tool

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -4,10 +4,11 @@ const EthereumStore = artifacts.require("EthereumStore");
 const EventFunction = artifacts.require("Function");
 const EventVerifier = artifacts.require("TriggerEventVerifier");
 
-const saveGasInfo = require("../test/helpers/utils").saveGas
+const benchmarkHelpers = require("../benchmark/helpers")
 const config = require("../benchmark/config.json")
 
 module.exports = async (deployer, network) => {
+  
   try {
       deployer.deploy(Ion, "0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177")
       .then((res) => writeGasToFile(res.transactionHash, "Deploy Ion"))
@@ -26,6 +27,7 @@ module.exports = async (deployer, network) => {
 };
 
 writeGasToFile = async (txHash, contractName) => {
+  let duration = "Not estimated"
   receipt = await web3.eth.getTransactionReceipt(txHash)
-  saveGasInfo(config.BENCHMARK_DEPLOYMENT_FILEPATH, txHash, contractName, receipt.cumulativeGasUsed)
+  benchmarkHelpers.saveStatsToFile(config.BENCHMARK_DEPLOYMENT_FILEPATH, txHash, contractName, receipt.cumulativeGasUsed, duration)
 }
