0b9c7d6fec990a6448816d8a239a3c387a171e14
andrea <dinennoandrea@gmail.com>
2020-02-11 11:12:15 +0000

    WIP test merkle tree

---
+++ test/merkle_tree_test.js
@@ -0,0 +1,24 @@
+const assert = require('assert');
+const path = require('path');
+const utils = require('./helpers/utils.js');
+const { MerkleTree } = require('./helpers/merkleTree.js');
+
+const MerkleTreeContract = artifacts.require("MerkleTreeTest");
+
+contract("Merkle Tree", (accounts) => {
+
+    const completeElements = [accounts[0], accounts[1], accounts[2], accounts[3]]
+    const sparseBinaryTree = [accounts[0], accounts[1], accounts[2]]
+
+    describe("Generate root", () => {
+        it("Should correctly calculate the merkle root", async () => {
+            const merkleTree = new MerkleTree(completeElements);
+            const root = merkleTree.getHexRoot();
+            console.log(root)
+
+            const merkleContract = await MerkleTreeContract.new()
+
+            receipt = await merkleContract.testRoot.call(merkleTree.elements)
+        })
+    })
+})
