fd8042e610263d39753784250cbd21bd045a39e1
andrea <dinennoandrea@gmail.com>
2020-02-12 10:56:52 +0000

    Test merkle tree library

--- test/merkle_tree_test.js
+++ test/merkle_tree_test.js
@@ -1,24 +1,73 @@
 const assert = require('assert');
 const path = require('path');
-const utils = require('./helpers/utils.js');
+const utils = require('./helpers/utils.js');          
 const { MerkleTree } = require('./helpers/merkleTree.js');
+const { keccak256, bufferToHex } = require('ethereumjs-util');
 
 const MerkleTreeContract = artifacts.require("MerkleTreeTest");
 
 contract("Merkle Tree", (accounts) => {
 
     const completeElements = [accounts[0], accounts[1], accounts[2], accounts[3]]
-    const sparseBinaryTree = [accounts[0], accounts[1], accounts[2]]
+    const allButOneElements = [accounts[0], accounts[1], accounts[2]]
+    const sparseElements = [accounts[0], accounts[1], accounts[2], accounts[3], accounts[4]]
 
     describe("Generate root", () => {
         it("Should correctly calculate the merkle root", async () => {
             const merkleTree = new MerkleTree(completeElements);
-            const root = merkleTree.getHexRoot();
-            console.log(root)
+            const expectedRoot = merkleTree.getHexRoot();
 
             const merkleContract = await MerkleTreeContract.new()
 
-            receipt = await merkleContract.testRoot.call(merkleTree.elements)
+            root = await merkleContract.testRoot.call(merkleTree.elements)
+            // console.log(expectedRoot, root)
+            assert.equal(root, expectedRoot)
+        })
+
+        it("Calculates the root with all but one leaves", async () => {
+            const merkleTree = new MerkleTree(allButOneElements);
+            const expectedRoot = merkleTree.getHexRoot();
+
+            const merkleContract = await MerkleTreeContract.new()
+
+            root = await merkleContract.testRoot.call(merkleTree.elements)
+            // console.log(expectedRoot, root)
+            assert.equal(root, expectedRoot)
+        })
+
+        it("Calculates the root with sparse leaves", async () => {
+            const merkleTree = new MerkleTree(sparseElements);
+            const expectedRoot = merkleTree.getHexRoot();
+
+            const merkleContract = await MerkleTreeContract.new()
+
+            root = await merkleContract.testRoot.call(merkleTree.elements)
+            // console.log(expectedRoot, root)
+            assert.equal(root, expectedRoot)
+        })
+
+
+        it("Calculates the root with one element", async () => {
+            const merkleTree = new MerkleTree([accounts[0]]);
+            const expectedRoot = merkleTree.getHexRoot();
+
+            const merkleContract = await MerkleTreeContract.new()
+
+            root = await merkleContract.testRoot.call(merkleTree.elements)
+            // console.log(expectedRoot, root)
+            assert.equal(root, expectedRoot)
+        })
+
+
+        it("Returns zero with no elements", async () => {
+            const merkleTree = new MerkleTree([]);
+            const expectedRoot = "0x0000000000000000000000000000000000000000000000000000000000000000";
+
+            const merkleContract = await MerkleTreeContract.new()
+
+            root = await merkleContract.testRoot.call(merkleTree.elements)
+            // console.log(expectedRoot, root)
+            assert.equal(root, expectedRoot)
         })
     })
 })
